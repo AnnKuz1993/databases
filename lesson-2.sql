de242b1316ffae96442ef5c91bf886dc5f6089b3
Anna <kaa33gr@mail.ru>
2020-05-24 12:49:36 +0400

    Commit 2. Create sample

--- lesson-2.sql
+++ lesson-2.sql
@@ -8,4 +8,12 @@ select * from users;
 insert into users values ('1', 'Anna');
 insert into users values ('2', 'Andrey');
 select * from users;
-    
+show databases;
+create database sample;    
+insert into users values ('3', 'Nina');
+select * from users;
+use example;
+insert into users values ('3', 'Nina');
+select * from users;
+use sample;
+select * from users;

88a8a9e00ba2ce6715dd7d626d1511c89522f71a
Anna <kaa33gr@mail.ru>
2020-06-19 16:08:46 +0400

    Commit 15. Update pandora (answer 1-5)

--- PANDORA.sql
+++ PANDORA.sql
@@ -30,14 +30,14 @@ create table accounts (
 	FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COMMENT = 'Личный кабинет';
     
-drop table if exists product_types;
 create table product_types (
 	id SERIAL PRIMARY KEY,
-    `name` ENUM ('шармы', 'браслеты', 'кольца', 'колье', 'кулоны', 'серьги'),
+    `name` varchar (100) not null COMMENT 'Тип',
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления записи',
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
     deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи'
 ) COMMENT = 'Типы продукции';
+
 
 drop table if exists products;
 create table products (
@@ -45,15 +45,17 @@ create table products (
     title varchar (255) NOT NULL COMMENT 'Наименование',
     desription TEXT NOT NULL COMMENT 'Характеристики',
     article varchar (150) NOT NULL COMMENT 'Артикул',
-    price_id BIGINT UNSIGNED NOT NULL,
+    price decimal (10,2) NOT NULL COMMENT 'Цена',
+    size int COMMENT 'Размер',
+    image_id BIGINT UNSIGNED NOT NULL,
     product_type_id BIGINT UNSIGNED NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления записи',
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
     deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи',
     
     INDEX products_isx (title),
-    FOREIGN KEY (price_id) REFERENCES price_list(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (product_type_id) REFERENCES product_types(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (product_type_id) REFERENCES product_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COMMENT = 'Номенклатура';
 
 drop table if exists images;
@@ -65,45 +67,35 @@ create table images (
     deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи'
 ) COMMENT = 'Изображение товара';
 
-drop table if exists price_list;
-create table price_list (
-	id serial primary key,
-    title varchar (255) NOT NULL COMMENT 'Наименование',
-    price decimal (10,2) NOT NULL COMMENT 'Цена',
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления записи',
-	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи'
-) COMMENT = 'Цена продукции';
-
 drop table if exists orders;
 create table orders (
 	 id SERIAL PRIMARY KEY,
      customer_id BIGINT UNSIGNED NOT NULL,
+     product_id BIGINT UNSIGNED NOT NULL, 
      created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления записи',
      updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
      deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи',
      
      INDEX customers_idx(customer_id),
-     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE ON UPDATE CASCADE
+     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COMMENT = 'Заказы';
 
 drop table if exists orders_products;
 create table orders_products (
 	id SERIAL PRIMARY KEY,
     order_id BIGINT UNSIGNED NOT NULL,
-    product_id BIGINT UNSIGNED NOT NULL,
     total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата добавления записи',
 	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
 	deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи',
     
-    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COMMENT = 'Состав заказа';
 
 drop table if exists stocks;
 create table stocks (
 	id SERIAL PRIMARY KEY,
-    customer_id BIGINT UNSIGNED NOT NULL,
     product_id BIGINT UNSIGNED NOT NULL,
     stock FLOAT COMMENT 'Величина скидки от 0.0 до 1.0',
     started_at DATETIME,
@@ -112,9 +104,7 @@ create table stocks (
 	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
 	deleted_at DATETIME NULL DEFAULT NULL COMMENT 'Дата удаления записи',
     
-    INDEX customers_idx(customer_id),
     INDEX products_idx(product_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) COMMENT = 'Скидки';
 
